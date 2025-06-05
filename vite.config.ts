8fcea8c94500b5b96bb0a7fa4d26a35c15b3b35e
mrirfanto <mrirfanto.work@gmail.com>
2025-06-05 18:44:03 +0700

    feat: config github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/github-repos-explorer/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
